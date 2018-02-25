85a96c7c5f2a1f108982bedff3ef1e828c08b37d
burilmishka <adutskm@gmail.com>
2018-02-25 23:45:46 +0300

    Zadolbalsia

--- src/index.js
+++ src/index.js
@@ -1,31 +1,78 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = new Array(); 
   }
 
   add(element) {
-    // your implementation
+   this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    // sortirovka indicies
+    var len = indices.length;
+    for (var i = 0; i < len-1; i++){
+      for (var j = i + 1; j < len; j++) {
+        if (indices[i] > indices[j]) {
+          let bolsh = indices[i];
+          indices[i] = indices[j];
+          indices[j] = bolsh;
+        }
+      }
+    }
+  if ( this.compareFunction !== undefined) {
+    var testFunction = this.compareFunction(1, 0);
+      var isReverse = false;
+      if (testFunction === -1) {
+        isReverse = true;
+      }
+    var len = indices.length;
+    for (var i = 0 ; i < len - 1; i++) {
+      for(var j = i + 1; j < len; j++) {
+        if ((this.compareFunction(this.array[indices[i]], 
+            this.array[indices[j]]) > 0 && !isReverse) || 
+            (this.compareFunction(this.array[indices[i]], 
+            this.array[indices[j]]) > 0 && isReverse)) {
+          let bolsh = this.array[indices[j]];
+          this.array[indices[i]] = this.array[indices[j]];
+          this.array[indices[j]] = bolsh;
+        }
+        else{
+          continue;
+        }
+      }
+    }
+  } 
+  else {
+    var len = indices.length;
+    for (var i = 0; i < len - 1; i++) {
+      for(var j = i + 1; j < len; j++) {
+        if ( this.array[indices[i]] <= this.array[indices[j]]) {
+          continue;
+        } 
+        else {
+          let bolsh = this.array[indices[j]];
+          this.array[indices[j]] = this.array[indices[i]];
+          this.array[indices[i]] = bolsh;
+        }
+      }
+    }
   }
 
-  setComparator(compareFunction) {
-    // your implementation
+  setComparator(compareFunction)
+    this.compareFunction = compareFunction;
+  }    
   }
-}
 
 module.exports = Sorter;
